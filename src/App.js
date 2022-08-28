8eeb0d46c076233aec5c4b345b8c276d8bd4afc0
Raul Rodrigues <raulferrodrigues@gmail.com>
2022-08-28 08:08:07 -0300

    simple video done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import React, { Component } from "react";
-import { test, graph } from "./astar/mapf"
-
+import { test, graph, graphSize, digest, video } from "./astar/mapf"
 
 class App extends Component {
 
   constructor(props) {
-    let agents = test()
     super(props)
+    this.state = { frame: [] }
+  }
 
-    this.agents = agents
-  } 
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
-  state = {
+  componentDidMount() {
+    this.frames = video()
+    this.frameCount = 0
     
-  };
+    this.setState({
+      frame: this.frames[0]
+    })
 
-
-
+    this.interval = setInterval(() => this.tick(), 200)
+  }
   
+  tick() {
+    if (this.frameCount < this.frames.length - 1) {
+      this.frameCount++
+
+      this.setState({
+        frame: this.frames[this.frameCount],
+        frameCount: this.frameCount
+      })
+    } else {
+
+      this.setState({
+        frame: this.frames[this.frames.length - 1],
+        frameCount: this.frameCount
+      })
+      
+      clearInterval(this.interval)
+    }    
+  }
+
+  grid() {
+    console.debug("state", this.state)
+    let count = 0
+
+    const listItems = this.state.frame.map((row) => {
+      const rowItems = row.map((cell) => {
+        count++ 
+        return <span className={cell} key={count}></span>
+      })
+      const rowDiv = <div className="row">{rowItems}</div>
+      return rowDiv
+    })
+
+    return listItems
+  }
+
   render() {
     return (
-      <div className="table">
-        {graph.grid.map((row) => {
-          
-          row.map((node) => {
-            console.debug("aqui")
-            return <div>oi</div>
-          })
-        })}
+      <div className="grid">
+        {this.grid()}
       </div>
     );
   }
--- src/astar/mapf.js
+++ src/astar/mapf.js
@@ -1,5 +1,6 @@
 import { astar, Graph } from "./astar.js";
-import { ten } from "./graph.js";
+import { example1 } from "./simTemplates.js";
+import * as deepcopy from 'deepcopy';
 
 /*
   DECISOES:
@@ -9,6 +10,10 @@ import { ten } from "./graph.js";
 */
 
 /* 
+- NAO TEM PROBLEMA DE DEADLOCK. PORQUE:
+  - porque temos uma ordem uni-direcional de prioridade
+  - prioridade decrescente (o agente com o maior numero é o mais importante e assim é o que continua se movendo. o outro para.)
+
 - ve essa historia de CBS
 
 - implementar um sistema de grid
@@ -25,35 +30,34 @@ import { ten } from "./graph.js";
 - o que fazer quando um agente está parado e bloquendo o objetivo do outro
 */
 
-const graph = new Graph(ten)
+const settings = example1()
+
+const graphSize = settings.graphSize
+const graph = settings.graph
+const agents = settings.agents
 
 function test() {
-  const agents = [
-    {
-      id: crypto.randomUUID(),
-      start: graph.grid[0][0],
-      end: graph.grid[9][9]
-    },
-    {
-      id: crypto.randomUUID(),
-      start: graph.grid[0][9],
-      end: graph.grid[9][0]
-    },
-  ]
+  graph.nodes[2].weight = 0
 
   for (let agent of agents) {
-    const path = astar.search(graph, agent.start, agent.end, undefined)
+    console.debug(agent.start, agent.end)
+    let path = []
+    path.push(agent.start)
+    path.push(...astar.search(graph, agent.start, agent.end, undefined))
+    
     agent.path = path
   }
 
   sim(agents)
 
-  return(agents)
+  return agents
 }
 
 function sim(agentsWithPaths) {
   let continueFlag = true
   let step = 0
+
+  console.debug("path", agentsWithPaths[0].path)
 
   while (continueFlag) {
     continueFlag = false
@@ -64,55 +68,209 @@ function sim(agentsWithPaths) {
         continueFlag = true
 
         locations.push({
-          id: agent.id,
-          position: {
-            x: agent.path[step].x,
-            y: agent.path[step].y
-          }
+          agent,
+          x: agent.path[step].x,
+          y: agent.path[step].y
         })
       }
     }
 
-    if (conflict(locations)) {
-      console.debug("conflict", locations)
+    const conflicts = conflict(locations)
+
+    if (conflicts.length !== 0) {
+      for (let collision of conflicts) {
+        let collisionNode
+        let previousCollisionNodeWeight
+
+        let highPriorityAgent
+        let lowPriorityAgent
+        
+        // Se a prioridade of agent foi maior que a do otherAgent, pausamos o otherAgent
+        if (collision.agent.agent.priority > collision.otherAgent.agent.priority) {
+          highPriorityAgent = collision.agent.agent
+          lowPriorityAgent = collision.otherAgent.agent
+        } else {
+          highPriorityAgent = collision.otherAgent.agent
+          lowPriorityAgent = collision.agent.agent
+        }
+
+        const bubble = lowPriorityAgent.path[step]
+        lowPriorityAgent.path.splice(step + 1, 0, bubble)
+
+        collisionNode = getNode(bubble.x, bubble.y, graph)
+        previousCollisionNodeWeight = collisionNode.weight
+        collisionNode.weight = 0
+
+        highPriorityAgent.path.length = step + 1
+        let newPathSegment = []
+        console.debug("new seg", highPriorityAgent.path[step], highPriorityAgent.end)
+        newPathSegment.push(...astar.search(graph, highPriorityAgent.path[step], highPriorityAgent.end, undefined))
+        highPriorityAgent.path.push(...newPathSegment)
+
+        collisionNode.weight = previousCollisionNodeWeight
+        console.debug("conflicts", conflicts)
+      }
     }
 
     step++
   }
+
+  return agentsWithPaths
 }
 
-function conflict(agentLocations) {
-  console.debug("agentLocations", agentLocations)
+/*
+1
+2
 
-  for (let agent of agentLocations) {
-    for (let otherAgent of agentLocations) {
-      if (agent.id !== otherAgent.id) {
-        observe(agent, otherAgent)
+*/
+
+function conflict(agentLocationPairs) {
+  let agentsWithConflicts = []
+
+  // Para todo o agente
+  for (let agentWithLocation of agentLocationPairs) {
+    // Pareando com todos os agentes
+    for (let otherAgentWithLocation of agentLocationPairs) {
+      // Se os dois agentes sendo pareados forem difirentes
+      if (agentWithLocation.agent.id !== otherAgentWithLocation.agent.id) {
+        // Faz os testes de conflito
+        const observed = observe(agentWithLocation, otherAgentWithLocation)
+        // Se houver conflitos
+        if (observed === 1) {
+          let skip = false
+          // Tenta não colocar conflitos do tipo A | B se ja tem B | A
+          for (let collision of agentsWithConflicts) {
+            if (collision.agent === otherAgentWithLocation && collision.otherAgent === agentWithLocation) {
+              skip = true
+            }
+          }
+
+          // Nao coloca duplicatas
+          if (!skip) {
+            agentsWithConflicts.push({
+              agent: agentWithLocation,
+              otherAgent: otherAgentWithLocation
+            })
+          }
+        }
       }
     }
   }
+
+  return agentsWithConflicts
+}
+
+function getNode(x, y, graph) {
+  return graph.nodes.find((el) => el.x === x && el.y === y)
 }
 
 function observe(agentA, agentB) {
   // north
-  if (  agentB.position.y === agentA.position.y - 1 &&
-        agentB.position.x === agentA.position.x
+  if (  agentB.y === agentA.y - 1 &&
+        agentB.x === agentA.x
   ) { return 1 }
 
   // south
-  if (  agentB.position.y === agentA.position.y + 1 && 
-        agentB.position.x === agentA.position.x
+  if (  agentB.y === agentA.y + 1 && 
+        agentB.x === agentA.x
   ) { return 1 }
 
   // west
-  if (  agentB.position.y === agentA.position.y &&
-        agentB.position.x === agentA.position.x - 1
+  if (  agentB.y === agentA.y &&
+        agentB.x === agentA.x - 1
   ) { return 1 }
 
   // east
-  if (  agentB.position.y === agentA.position.y &&
-        agentB.position.x === agentA.position.x + 1
+  if (  agentB.y === agentA.y &&
+        agentB.x === agentA.x + 1
   ) { return 1 }
 }
 
-export { test, graph };
+function digest() {
+  let finalGrid = []
+  for (let y = 0; y < graphSize; y++) {
+    let row = []
+    for (let x = 0; x < graphSize; x++) {
+      row.push("empty")
+    }
+    finalGrid.push(row)
+  }
+
+  const agentsWithPaths = test()
+
+  let continueFlag = true
+  let step = 0
+  while (continueFlag) {
+    continueFlag = false
+
+    for (let agent of agentsWithPaths) {
+      if (step < agent.path.length) {
+        continueFlag = true
+
+        finalGrid[agent.path[step].y][agent.path[step].x] = agent.color
+      }
+    }
+
+    step++
+  }
+
+  return finalGrid
+}
+
+function video() {
+  let frames = []
+  let initialFrame = []
+
+  frames.push(initialFrame)
+
+  // Produz of frame inicial
+  for (let y = 0; y < graphSize; y++) {
+    let row = []
+    for (let x = 0; x < graphSize; x++) {
+      row.push("empty")
+    }
+    initialFrame.push(row)
+  }
+
+  // Roda a simulação
+  for (let agent of agents) {
+    // console.debug(agent.start, agent.end)
+    let path = []
+    path.push(agent.start)
+    path.push(...astar.search(graph, agent.start, agent.end, undefined))
+    
+    agent.path = path
+  }
+
+  // Agentes com os caminhos
+  let agentsWithPaths = sim(agents)
+
+  // Estruturas para produzir o video
+  let continueFlag = true
+  let step = 0
+  let lastFrame = deepcopy(initialFrame)
+
+  // Produz o video
+  while (continueFlag) {
+    continueFlag = false
+    let newFrame = deepcopy(initialFrame)
+
+    for (let agent of agentsWithPaths) {
+      if (step < agent.path.length) {
+        continueFlag = true
+
+        // fazer novo frame
+        newFrame[agent.path[step].y][agent.path[step].x] = agent.color
+      }
+    }
+
+    frames.push(newFrame)
+    lastFrame = newFrame
+
+    step++
+  }
+
+  return frames
+}
+
+export { test, graph, graphSize, digest, video };
